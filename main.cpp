26283ef8f03d79daecb239c28d6048ee09193c13
Nishat <nts7434@nyu.edu>
2025-11-08 20:33:43 -0500

    added heading

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: Nishat Shamiha
+* Assignment: Rise of the AI
+* Date due: 2025-11-08, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #include "CS3113/End.h"
 
 // Global Constants
